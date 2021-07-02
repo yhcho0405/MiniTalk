cc4066728607cbc09a50ae4729f11bd81ac72022
yhcho0405 <yhcho0405@kakao.com>
2021-07-02 18:58:48 +0900

    bonus

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -6,11 +6,14 @@
 /*   By: youncho <youncho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 17:10:38 by youncho           #+#    #+#             */
-/*   Updated: 2021/06/29 17:12:45 by youncho          ###   ########.fr       */
+/*   Updated: 2021/07/02 18:57:12 by youncho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
+
+# include "libft/libft.h"
+# include <signal.h>
 
 #endif
